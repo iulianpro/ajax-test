02586767feb858fc15baca9f4e7db15c0f89f15d
iulianpro <iulian.prodea@gmail.com>
2021-09-25 22:01:04 +0100

    Update

--- main.js
+++ main.js
@@ -15,20 +15,21 @@ function getTableHeaders(obj) {
     var tableHeaders = [];
 
     Object.keys(obj).forEach(function (key) {
-        tableHeaders.push(`<td>${key}</td>`)
+        tableHeaders.push(`<td class="text-uppercase">${key}</td>`)
     });
+    console.log(Object)
 
     return `<tr>${tableHeaders}</tr>`;
 }
 
 function generatePaginationButtons(next, prev) {
     if (next && prev) {
-        return `<button onclick="writeToDocument('${prev}')">Previous</button>
-                <button onclick="writeToDocument('${next}')">Next</button>`;
+        return `<button class="btn btn-primary my-3 ml-5" onclick="writeToDocument('${prev}')">Previous</button>
+                <button class="btn btn-primary my-3 ml-2" onclick="writeToDocument('${next}')">Next</button>`;
     } else if (next && !prev) {
-        return `<button onclick="writeToDocument('${next}')">Next</button>`;
+        return `<button class="btn btn-primary my-3 ml-5" onclick="writeToDocument('${next}')">Next</button>`;
     } else if (!next && prev) {
-        return `<button onclick="writeToDocument('${prev}')">Previous</button>`;
+        return `<button class="btn btn-primary my-3 ml-5" onclick="writeToDocument('${prev}')">Previous</button>`;
     }
 }
 
@@ -44,18 +45,19 @@ function writeToDocument(url) {
         }
         data = data.results;
         var tableHeaders = getTableHeaders(data[0]);
+        console.log(data);
 
         data.forEach(function (item) {
             var dataRow = [];
 
             Object.keys(item).forEach(function (key) {
                 var rowData = item[key].toString();
-                var truncateData = rowData.substring(0, 15);
-                dataRow.push(`<td>${truncateData}</td>`);
+                var truncateData = rowData.substring(0, 10);
+                dataRow.push(`<td class="p-2">${truncateData}</td>`);
             });
             tableRows.push(`<tr>${dataRow}</tr>`)
         });
 
-        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`.replace(/,/g, '');
+        el.innerHTML = `<table class="table table-bordered">${tableHeaders}${tableRows}</table>${pagination}`.replace(/,/g, '');
     });
 }
